392d9d08735d968871181046a341e231f8dcdbac
tomek <2002tomo@gmail.com>
2024-12-07 14:29:11 +0100

    initial commit backend

---
+++ MartianCalendarBackend/src/main/java/org/bonsai/martiancalendarbackend/MartianCalendarBackendApplication.java
@@ -0,0 +1,13 @@
+package org.bonsai.martiancalendarbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MartianCalendarBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MartianCalendarBackendApplication.class, args);
+    }
+
+}
---
+++ MartianCalendarBackend/src/test/java/org/bonsai/martiancalendarbackend/MartianCalendarBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.bonsai.martiancalendarbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MartianCalendarBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
